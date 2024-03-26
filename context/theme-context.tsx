db4986a8dbab995fe42fcf681d80848b677513c9
ttiimmothy <dinosauli2006@gmail.com>
2024-03-25 20:21:58 -0400

    feat: disable checking the window prefers dark color scheme in theme context

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -34,17 +34,16 @@ export default function ThemeContextProvider({
 
   useEffect(() => {
     const localTheme = window.localStorage.getItem("theme") as Theme | null;
-
     if (localTheme) {
       setTheme(localTheme);
-
       if (localTheme === "dark") {
         document.documentElement.classList.add("dark");
       }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
     }
+    // else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    //   setTheme("dark");
+    //   document.documentElement.classList.add("dark");
+    // }
   }, []);
 
   return (
